3f700e60fff55fa6241a1be07d91b157a0264185
Akshay Das <63033525+akshay98322@users.noreply.github.com>
2022-05-04 18:23:18 +0530

    Added custom user model

--- dj_api_auth/settings.py
+++ dj_api_auth/settings.py
@@ -4,6 +4,8 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+AUTH_USER_MODEL = 'account.User'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
